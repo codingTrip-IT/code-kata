157015efd755bc29cd50fd012ffa9b53e18968ee
YEIN JO <83770197+codingTrip-IT@users.noreply.github.com>
2024-12-24 10:02:25 +0900

    Add files via upload

--- Ex4.java
+++ Ex4.java
@@ -3,7 +3,7 @@ public class Ex4 {
         public int solution(int age) {
             int answer = 0;
             int year = 2022;
-            if(age>=0 && age <=120){
+            if(age>0 && age <=120){
                 answer = year-age+1;
             }
             return answer;
