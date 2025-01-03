7a4c9876060f650931db43aaf099f412d95b21c5
YEIN JO <83770197+codingTrip-IT@users.noreply.github.com>
2025-01-03 09:07:49 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.04 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        
+//         for(int i=0;i<s.length();i++){
+            
+//         }
+        return answer;
+    }
+}
