a944db6492571155185865592e6688ddb7f7fead
YEIN JO <83770197+codingTrip-IT@users.noreply.github.com>
2025-02-28 09:42:47 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 5.30 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] numbers) {
             int[] answer = {};
-            Set<Integer> set = new HashSet<>();
+            Set<Integer> set = new TreeSet<>();
             
             for (int i = 0; i < numbers.length; i++) {
                 for (int j = 0; j < numbers.length; j++) {
@@ -13,9 +13,7 @@ class Solution {
                 }
             }
 
-            List<Integer> setList = new ArrayList<>(set);
-            Collections.sort(setList);
-            answer = setList.stream().mapToInt(Integer::intValue).toArray();
+            answer = set.stream().mapToInt(Integer::intValue).toArray();
             return answer;
     }
 }
