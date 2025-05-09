74911a1b994f76901745e0be697e481effd8f56b
codingTrip <83770197+codingTrip-IT@users.noreply.github.com>
2025-05-09 10:29:09 +0900

    [level 0] Title: 배열의 평균값, Time: 0.05 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/README.md
+++ 프로그래머스/0/120817. 배열의 평균값/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열의 평균값 - 120817 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120817?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120817) 
 
 ### 성능 요약
 
-메모리: 85.2 MB, 시간: 0.04 ms
+메모리: 86.4 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 26일 09:43:55
+2025년 05월 09일 10:29:08
 
 ### 문제 설명
 
--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -1,19 +1,10 @@
 class Solution {
     public double solution(int[] numbers) {
         double answer = 0;
-        double sum = 0;
-        double avg = 0;
-        
-        if(numbers.length>=1 && numbers.length<=100){
-            for(int num:numbers) {
-                if(num>=0 && num<=1000){
-                    sum += num;
-                }
-            }
-            
-            answer = sum/numbers.length;
-
+        for (int number : numbers) {
+            answer += number;
         }
+        answer /= numbers.length;
         return answer;
     }
 }
