141e16097e0b17dfacadf1d547e8b634d5b42770
codingTrip <83770197+codingTrip-IT@users.noreply.github.com>
2025-05-13 10:34:06 +0900

    [level 1] Title: 평균 구하기, Time: 4.27 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,16 +1,10 @@
+import java.util.Arrays;
+
 class Solution {
     public double solution(int[] arr) {
         double answer = 0;
-        double sum = 0;
-        
-        if(arr.length>=1 && arr.length<=100){
-            for(int i : arr){
-                if(i>=-10000 && i<=10000){
-                    sum += i;
-                }
-            }
-            answer = sum / arr.length;
-        }
+        answer = Arrays.stream(arr).average().getAsDouble();
+        System.out.println(answer);
         return answer;
     }
 }
